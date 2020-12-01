368ba297bcacb5116a881f0598293f8957b8a116
professorisidro <isidro@professorisidro.com.br>
2020-11-30 22:26:29 -0300

    tirando runwith

--- src/test/java/br/com/professorisidro/testador/unit/SimpleTests.java
+++ src/test/java/br/com/professorisidro/testador/unit/SimpleTests.java
@@ -1,16 +1,13 @@
 package br.com.professorisidro.testador.unit;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;	
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import br.com.professorisidro.testador.model.Cliente;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 public class SimpleTests {
 	
 	@Test
